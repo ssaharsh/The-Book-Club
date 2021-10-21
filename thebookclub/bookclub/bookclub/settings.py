999e85093835b376955d764bb5df437b9f2447d4
Saharsh Sodani <saharshsodani2000@gmail.com>
2021-10-21 14:14:53 +0530

    added product and category

--- thebookclub/bookclub/bookclub/settings.py
+++ thebookclub/bookclub/bookclub/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
