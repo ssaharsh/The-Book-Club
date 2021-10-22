2a028fe0ea93a2e459ecb3c5d603e751e953e6f0
Saharsh Sodani <saharshsodani2000@gmail.com>
2021-10-22 21:29:23 +0530

    form formatted

--- thebookclub/bookclub/users/forms.py
+++ thebookclub/bookclub/users/forms.py
@@ -1,12 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import widgets
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+        widgets={
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'first_name':forms.TextInput(attrs={'class':'form-control'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+        }
